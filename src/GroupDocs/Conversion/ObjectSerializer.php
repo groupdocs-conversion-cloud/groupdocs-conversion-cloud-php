d0667cfbab847f06086c01ca102ad54d807449ce
Bert Brunekreeft <BBrunekreeft@gmail.com>
2021-11-23 14:50:47 +0100

    Prevent exception
    Exception "Trying to access array offset on value of type int" is thrown.

--- src/GroupDocs/Conversion/ObjectSerializer.php
+++ src/GroupDocs/Conversion/ObjectSerializer.php
@@ -169,12 +169,15 @@ class ObjectSerializer
      */
     public static function toString($value)
     {
-        if (date(\DATE_ATOM, preg_match("/^[1-9][0-9]*$/", $value)[0]) instanceof \DateTime) { // datetime in ISO8601 format
-            $datetime = preg_match("/^[1-9][0-9]*$/", $value)[0];
-            return date(\DATE_ATOM, $datetime);
-        } else {
+        try {
+            if (date(\DATE_ATOM, preg_match("/^[1-9][0-9]*$/", $value)[0]) instanceof \DateTime) { // datetime in ISO8601 format
+                $datetime = preg_match("/^[1-9][0-9]*$/", $value)[0];
+                return date(\DATE_ATOM, $datetime);
+            }
+        } catch (\Exception $e) {
             return $value;
         }
+        return $value;
     }
 
     /*
